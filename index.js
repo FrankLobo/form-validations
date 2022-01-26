0a4ff9bb21a8070af09433cc59b0c6a6a7a728c4
FrankLobo <frankeloboq@gmail.com>
2022-01-26 21:52:04 +0000

    cambio 4

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// const { doc } = require("prettier");
+
 let paymentForm = document.getElementById("payform");
 
 paymentForm.addEventListener("submit", (event) => {
@@ -11,33 +13,50 @@ paymentForm.addEventListener("submit", (event) => {
 
   if (form.card.value === "" || !numbersOnly.test(form.card.value)) {
     allGood = false;
+    form.card.style.backgroundColor = "#F8D7DA";
   }
   if (form.cvc.value === "" || !cvcNumbers.test(form.cvc.value)) {
     allGood = false;
+    form.cvc.style.backgroundColor = "#F8D7DA";
   }
   if (form.amount.value === "" || !numbersOnly.test(form.amount.value)) {
     allGood = false;
+    form.amount.style.backgroundColor = "#F8D7DA";
   }
   if (form.firstname.value === "" || !charactersOnly.test(form.firstname.value)) {
     allGood = false;
+    form.firstname.style.backgroundColor = "#F8D7DA";
   }
   if (form.lastname.value === "" || !charactersOnly.test(form.lastname.value)) {
     allGood = false;
+    form.lastname.style.backgroundColor = "#F8D7DA";
   }
   if (form.city.value === "" || !charactersOnly.test(form.city.value)) {
     allGood = false;
+    form.city.style.backgroundColor = "#F8D7DA";
   }
   if (form.state.value === "Pick a state" || !charactersOnly.test(form.state.value)) {
     allGood = false;
+    form.state.style.backgroundColor = "#F8D7DA";
   }
   if (form.postalcode.value === "" || !numbersOnly.test(form.postalcode.value)) {
     allGood = false;
+    form.postalcode.style.backgroundColor = "#F8D7DA";
   }
   if (form.message.value === "" || !charactersOnly.test(form.message.value)) {
     allGood = false;
+    form.message.style.backgroundColor = "#F8D7DA";
   }
 
   if (allGood) {
     form.submit();
-  } else alert("verify the form");
+  } else if(!allGood){
+   document.getElementById('alertmissinginfo').style.display = "block";
+  }
+  
 });
+
+paymentForm.addEventListener('reset', () => {
+    alertMissingInfo = document.getElementById('alertmissinginfo').style.display = "none";
+})
+let alertMissingInfo = document.getElementById('alertmissinginfo').style.display = "none";

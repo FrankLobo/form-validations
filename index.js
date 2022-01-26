afd20c48f03d5eed682cbda88a9754e82828c998
FrankLobo <frankeloboq@gmail.com>
2022-01-26 18:32:12 +0000

    cambios 2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let paymentForm = document.getElementById("payform");
 
-paymentForm.addEventListener("submit", e => {
+paymentForm.addEventListener("submit", (e) => {
   // event.preventDefault();
   let allGood = true;
   const form = e.target;
 
   let numbersOnly = /^[0-9]+$/;
-  let charactersOnly = /^[a-z]+$/;
-  let cvcNumbers = /^[0-9]{3}$/;
+  let cvcNumbers =  /^[0-9]{3}$/;
+  let charactersOnly = /^[a-zA-Z\s]+$/;
 
   if (form.card.value === "" || !numbersOnly.test(form.card.value)) {
     allGood = false;
@@ -15,4 +15,12 @@ paymentForm.addEventListener("submit", e => {
   if (form.cvc.value === "" || !cvcNumbers.test(form.cvc.value)) {
     allGood = false;
   }
+  if (form.amout.value === "" || !charactersOnly.test(form.amout.value)) {
+      allGood = false;
+  }
+if (form.firstname.value === "" || !charactersOnly.test(form.firstname.value)) {
+    allGood = false;
+}
+
+
 });

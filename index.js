968b037fb4d713c83f87021632fbfe93ed099389
FrankLobo <frankeloboq@gmail.com>
2022-01-26 19:52:08 +0000

    cambios 3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let paymentForm = document.getElementById("payform");
 
-paymentForm.addEventListener("submit", (e) => {
-  // event.preventDefault();
+paymentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
   let allGood = true;
-  const form = e.target;
+  const form = event.target;
 
   let numbersOnly = /^[0-9]+$/;
-  let cvcNumbers =  /^[0-9]{3}$/;
+  let cvcNumbers = /^[0-9]{3}$/;
   let charactersOnly = /^[a-zA-Z\s]+$/;
 
   if (form.card.value === "" || !numbersOnly.test(form.card.value)) {
@@ -15,12 +15,29 @@ paymentForm.addEventListener("submit", (e) => {
   if (form.cvc.value === "" || !cvcNumbers.test(form.cvc.value)) {
     allGood = false;
   }
-  if (form.amout.value === "" || !charactersOnly.test(form.amout.value)) {
-      allGood = false;
-  }
-if (form.firstname.value === "" || !charactersOnly.test(form.firstname.value)) {
+  if (form.amount.value === "" || !numbersOnly.test(form.amount.value)) {
     allGood = false;
-}
+  }
+  if (form.firstname.value === "" || !charactersOnly.test(form.firstname.value)) {
+    allGood = false;
+  }
+  if (form.lastname.value === "" || !charactersOnly.test(form.lastname.value)) {
+    allGood = false;
+  }
+  if (form.city.value === "" || !charactersOnly.test(form.city.value)) {
+    allGood = false;
+  }
+  if (form.state.value === "Pick a state" || !charactersOnly.test(form.state.value)) {
+    allGood = false;
+  }
+  if (form.postalcode.value === "" || !numbersOnly.test(form.postalcode.value)) {
+    allGood = false;
+  }
+  if (form.message.value === "" || !charactersOnly.test(form.message.value)) {
+    allGood = false;
+  }
 
-
+  if (allGood) {
+    form.submit();
+  } else alert("verify the form");
 });
